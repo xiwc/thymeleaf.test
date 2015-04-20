b779e03af12a64cf8ef35156faa5259c34d5d4c9
xiwc <xiwc87@yeah.net>
2015-04-20 17:13:51 +0800

    jQuery.Deferred & jQuery.Callbacks testing.

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function($) {
+	
 	$.fn.api.settings.api = {
 		'test' : 'dp/test?flg={/flg}'
 	};
@@ -38,4 +39,62 @@ jQuery(document).ready(function($) {
 			console.log('onComplete' + response);
 		}
 	});
+});
+
+jQuery(document).ready(function($) {
+	
+	$.get("dp/test").done(function(resp){
+		console.log(JSON.stringify(resp));
+	});
+	
+	
+	var d = (function(){
+		
+		var d = jQuery.Deferred();
+		
+		setTimeout(function(){
+			d.resolve({success:true});
+		}, 5000);
+		
+		return d.promise();
+		
+	})();
+	
+	d.done(function(resp){
+		console.log(JSON.stringify(resp));
+	});
+	
+	console.log("...");
+	
+	// ----------------------------------------------
+	
+	/**
+	 * var c = jQuery.Callbacks(flags);
+	 * 可用的 flags: 
+		once: 确保这个回调列表只执行一次(像一个递延 Deferred). 
+		memory: 保持以前的值和将添加到这个列表的后面的最新的值立即执行调用任何回调 (像一个递延 Deferred). 
+		unique: 确保一次只能添加一个回调(所以有没有在列表中的重复). 
+		stopOnFalse: 当一个回调返回false 时中断调用,默认情况下，回调列表将像事件的回调列表中可以多次触发。
+	 */
+	var c = jQuery.Callbacks();
+	
+	c.add(function(val){
+		console.log("1:" + val);
+	});
+	
+	c.fire('test callbacks');
+	
+	//c.empty();
+	//c.disable();
+	//c.lock();
+	//c.has(function(){});
+	//c.remove(function(){});
+	
+	c.add(function(val){
+		console.log("2:" + val);
+	}).add(function(val){
+		console.log("3:" + val);
+	});
+	
+	c.fire('test callbacks');
 });
