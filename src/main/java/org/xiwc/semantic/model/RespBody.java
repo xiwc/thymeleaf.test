b3de71b18292111c98b2919085f71b6947bbcfbf
weicheng xi <xiwc87@yeah.net>
2015-04-24 21:27:49 +0800

    1. implement the image file upload store 2. add test profile for custom properties

--- src/main/java/org/xiwc/semantic/model/RespBody.java
+++ src/main/java/org/xiwc/semantic/model/RespBody.java
@@ -7,10 +7,14 @@ public class RespBody implements Serializable {
 	/** serialVersionUID long */
 	private static final long serialVersionUID = -1965817463440121331L;
 	public boolean success;
-	public String message;
 	public Object data;
 
-	public RespBody() {
+	public static RespBody succeed(Object data) {
+		return new RespBody(true, data);
+	}
+
+	public static RespBody failed(Object data) {
+		return new RespBody(false, data);
 	}
 
 	public RespBody(boolean success) {
@@ -22,31 +26,12 @@ public class RespBody implements Serializable {
 		this.data = data;
 	}
 
-	public RespBody(boolean success, String message) {
-		this.success = success;
-		this.message = message;
-	}
-
-	public RespBody(boolean success, String message, Object data) {
-		this.success = success;
-		this.message = message;
-		this.data = data;
-	}
-
 	public boolean isSuccess() {
 		return success;
 	}
 
 	public void setSuccess(boolean success) {
 		this.success = success;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
 	}
 
 	public Object getData() {
--- src/main/java/org/xiwc/semantic/web/PageController.java
+++ src/main/java/org/xiwc/semantic/web/PageController.java
@@ -1,13 +1,17 @@
 package org.xiwc.semantic.web;
 
+import java.io.File;
 import java.util.Locale;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -19,6 +23,8 @@ import org.springframework.web.multipart.MultipartFile;
 import org.xiwc.semantic.entity.Test;
 import org.xiwc.semantic.entity.TestRepository;
 import org.xiwc.semantic.model.RespBody;
+import org.xiwc.semantic.util.ImageUtil;
+import org.xiwc.semantic.util.StringUtil;
 import org.xiwc.semantic.util.WebUtil;
 
 @Controller
@@ -26,6 +32,9 @@ import org.xiwc.semantic.util.WebUtil;
 public class PageController {
 
 	static Logger logger = LoggerFactory.getLogger(PageController.class);
+
+	@Autowired
+	Environment env;
 
 	@Autowired
 	TestRepository testRepository;
@@ -58,8 +67,68 @@ public class PageController {
 
 		logger.debug("upload file start...");
 
-		String ctxRealPath = WebUtil.getRealPath(request);
+		String realPath = WebUtil.getRealPath(request);
 
-		return new RespBody(true, "img/img01.jpg");
+		if (file == null || file.isEmpty()) {
+			return RespBody.failed("上传文件为空！");
+		}
+
+		String originalFilename = file.getOriginalFilename();
+
+		if (!ImageUtil.isImage(originalFilename)) {
+			return RespBody.failed("上传文件不是图片！");
+		}
+
+		String type = originalFilename.substring(originalFilename
+				.lastIndexOf("."));
+
+		String fileName = StringUtil.replace("{?1}{?2}", UUID.randomUUID(),
+				type);
+
+		String storePath = env.getProperty("test.upload.img.store.path");
+		int sizeOriginal = env.getProperty(
+				"test.upload.img.scale.size.original", Integer.class);
+		int sizeLarge = env.getProperty("test.upload.img.scale.size.large",
+				Integer.class);
+		int sizeHuge = env.getProperty("test.upload.img.scale.size.huge",
+				Integer.class);
+
+		// relative file path
+		String path = storePath + sizeOriginal + "/" + fileName;// 原始图片存放
+		String pathLarge = storePath + sizeLarge + "/" + fileName;// 缩放图片存放
+		String pathHuge = storePath + sizeHuge + "/" + fileName;// 缩放图片存放
+
+		// absolute file path
+		String filePath = realPath + path;
+
+		try {
+
+			// make upload dir if not exists
+			FileUtils.forceMkdir(new File(realPath + storePath + sizeOriginal));
+			FileUtils.forceMkdir(new File(realPath + storePath + sizeLarge));
+			FileUtils.forceMkdir(new File(realPath + storePath + sizeHuge));
+
+			// store into webapp dir
+			file.transferTo(new File(filePath));
+
+			// scale image size as thumbnail
+			// 图片缩放处理.120*120
+			ImageUtil.scale2(filePath, realPath + pathLarge, sizeLarge,
+					sizeLarge, true);
+			// 图片缩放处理.640*640
+			ImageUtil.scale2(filePath, realPath + pathHuge, sizeHuge, sizeHuge,
+					true);
+
+			// 保存记录到数据库
+			// TODO
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error(e.getMessage(), e);
+			return RespBody.failed(e.getMessage());
+		}
+
+		// back relative file path
+		return RespBody.succeed(storePath + sizeOriginal + "/" + fileName);
 	}
 }
